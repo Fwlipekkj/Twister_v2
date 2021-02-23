5aa1e3f71d97959dc27b341580e50733be3440c2
Felipe Costa <felipehscbr@gmail.com>
2021-02-23 11:40:39 -0300

    added more posts to test kaminari

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -8,6 +10,12 @@
 
 user = User.find_or_create_by(name:"Felipe", nickname:"fefe", email:"felipe@gmail.com", password:"102030fefe")
 
+post = Post.find_or_create_by(message:Faker::Lorem.sentence, user:user)
+post = Post.find_or_create_by(message:Faker::Lorem.sentence, user:user)
+post = Post.find_or_create_by(message:Faker::Lorem.sentence, user:user)
+post = Post.find_or_create_by(message:Faker::Lorem.sentence, user:user)
+post = Post.find_or_create_by(message:Faker::Lorem.sentence, user:user)
+post = Post.find_or_create_by(message:Faker::Lorem.sentence, user:user)
 post = Post.find_or_create_by(message:Faker::Lorem.sentence, user:user)
 post = Post.find_or_create_by(message:Faker::Lorem.sentence, user:user)
 post = Post.find_or_create_by(message:Faker::Lorem.sentence, user:user)
